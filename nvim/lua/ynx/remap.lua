6e28647ea046f35f605fba9d3f4810fa7a2642f3
Martin J <per.martin@live.se>
2023-02-25 14:47:27 +0100

    Added maps for tabs

--- nvim/lua/ynx/remap.lua
+++ nvim/lua/ynx/remap.lua
@@ -1,2 +1,7 @@
+-- Set leader key to space 
 vim.g.mapleader = " "
+-- Set how we exit a file and return to dir view..
 vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+
+require('ynx.remap.window')
+require('ynx.remap.tabs')
