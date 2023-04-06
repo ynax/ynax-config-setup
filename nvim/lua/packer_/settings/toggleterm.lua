a4ca2e54aa727ef861b0e59816bd4b68abc48524
maritn j <per.martin@live.se>
2023-04-06 12:23:18 +0200

    comfy changes

--- nvim/lua/packer_/settings/toggleterm.lua
+++ nvim/lua/packer_/settings/toggleterm.lua
@@ -4,7 +4,7 @@ require('toggleterm').setup{
 	shade_terminals = true,
 	-- ?? 
 	hide_numbers = false, -- hide the number column in toggleterm buffers
-	start_in_insert = false, -- start in normal mode
+	start_in_insert = true, -- start in normal mode
 }
 
 
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -23,7 +23,7 @@ vim.o.termguicolors = true
 vim.opt.termguicolors = true
 vim.opt.list = false -- Needed to make indent_blankline awesome! 
 
-vim.opt.colorcolumn = "80" -- Line at X:s column
+vim.opt.colorcolumn = "100" -- Line at X:s column
 
 -- Specify split directions
 vim.o.splitbelow = true
